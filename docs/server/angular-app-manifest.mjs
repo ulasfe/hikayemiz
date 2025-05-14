479e060ee6a87a1b51f1826d2dd93c09e230293d
Ulas Demir <ulas.fe@gmail.com>
2025-05-15 01:53:19 +0300

    Deploy via docs for GitHub Pages

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 2901, hash: 'd1d8b15a50419a4dd8691dc62d8d80cd9a41d10c96d92c016e218a340508606e', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1292, hash: '71d4be517334bf520543927179fdb6d1f56bcdb8b8aad80427c1a204e544209d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 23726, hash: '509dde115700b04e0e2577be40352b3dd9d2b4bb32f2c488fff6bbda550136c9', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 2933, hash: 'd0f29133e54251050eb64b6d8096080e0cb0bc576972e501be6051ff2814b98c', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1327, hash: '59f90424d854d6e6fb4f9c6c9d5f751df014c765442728beab442be30d725d3d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 23755, hash: 'beadd40df7bfb95b4a8a9b8822e1e3b5fed2eec384c86346724f080cf2a544c3', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-BGW55XLI.css': {size: 12205, hash: 'UeqttLX4fBY', text: () => import('./assets-chunks/styles-BGW55XLI_css.mjs').then(m => m.default)}
   },
 };
