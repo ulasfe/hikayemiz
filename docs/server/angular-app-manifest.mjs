15077a8299bd5ee9158bbc724c5d97769b5729ec
Ulas Demir <ulas.fe@gmail.com>
2025-05-15 01:29:39 +0300

    Docs dosyaları yeniden düzenlendi

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 2932, hash: '8248aaa054a4f1c4df427f19599124b19ecaa6f16751a8db571ea9e2a01caeed', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1326, hash: '92423a1ea8db25cf70c53e7739952af83f98209b53fd3249715594cc79c0e4f5', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 23754, hash: 'c631280287c26ea5386bff38ac31e4d95bdda29e3f6be17224e340621889633a', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 2900, hash: '1d8c81cda8a5fde4cba137ee7203ddce520858e6330a4c1e65ebf71b0b9f1923', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1291, hash: '1ae2691f522deb36860401af614f62036d742e3539ce7b61e9dbb0a0157e40bb', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 23725, hash: '43946cf2b517a60939d2b7d360d40fa490c707fbf58992e1d4a26fcfcc36f673', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-BGW55XLI.css': {size: 12205, hash: 'UeqttLX4fBY', text: () => import('./assets-chunks/styles-BGW55XLI_css.mjs').then(m => m.default)}
   },
 };
